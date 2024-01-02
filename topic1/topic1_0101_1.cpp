75f637eb122c1b47040e93258f908fd636c9b917
임규연 <lky473736@icloud.com>
2024-01-02 22:01:05 +0900

    upload) warm up and setting the code env
    using xeus-cling and code-runner at vscode

---
+++ topic1/topic1_0101_1.cpp
@@ -0,0 +1,56 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stderr",
+     "output_type": "stream",
+     "text": [
+      "/var/folders/_z/gryfr07n59jgb3wrd062h1ym0000gn/T/tmp63e13dxj.c:1:10: fatal error: 'iostream' file not found\n",
+      "#include <iostream>\n",
+      "         ^~~~~~~~~~\n",
+      "1 error generated.\n",
+      "[C kernel] GCC exited with code 1, the executable will not be executed"
+     ]
+    }
+   ],
+   "source": [
+    "#include <iostream>\n",
+    "using namespace std;\n",
+    "\n",
+    "int a;\n",
+    "a=9;\n",
+    "cout<<a;"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "markdown",
+   "metadata": {},
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "C",
+   "language": "c",
+   "name": "c"
+  },
+  "language_info": {
+   "file_extension": ".c",
+   "mimetype": "text/plain",
+   "name": "c"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
