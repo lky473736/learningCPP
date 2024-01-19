1a7a10d8417b555406321c7eeb08dfaf911267f6
임규연 <lky473736@icloud.com>
2024-01-19 23:49:19 +0900

    upload) practice dereferencing

---
+++ topic3/topic3_0119_4.cpp
@@ -0,0 +1,14 @@
+// 역참조 연습
+
+#include <iostream>
+
+int main()
+{
+    double high_temp = 100.7;
+    double low_temp = 37.4;
+    double *temp_ptr = &high_temp;
+    
+    std::cout << *temp_ptr << std::endl; // 100.7
+    temp_ptr = &low_temp;
+    std::cout << *temp_ptr << std::endl; // 37.4
+}
